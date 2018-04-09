4640b68438509c38f4cd8291cc77dca76a24b9d8
Jakub Palarik <palarik.jakub@gmail.com>
2018-04-09 05:07:45 +0200

    save

--- src/Testy/Kvíz.java
+++ src/Testy/Kvíz.java
@@ -6,6 +6,8 @@ public class Kvíz{
 	public String[] kluc;
 	public String[] otazky;
 	public String odpoved;
+	public int body;
+	public int cisotazka;
 	
 	public Kvíz(String[] k, String[] ot, String od){
 		
@@ -13,24 +15,29 @@ public class Kvíz{
 	this.otazky = ot;
 	this.odpoved = od;
 		
-	        int correct = 0;
-	        int incorrect = 0;
+	            
+	 public static int boduj() {
+		 
+		 int spravne = 0;
+	     
+	            if (odpoved.compareTo(kluc[cisotazka])==0)
+	                spravne++;
+	           
+	 				return spravne;
+	 }
+	        
+	 for (cisotazka = 0; cisotazka < 20; cisotazka++){
+         System.out.println(otazky[cisotazka]);
+         }
 
-	        for (int cisotazka = 0; cisotazka < 20; cisotazka++){
-	            System.out.println(otazky[cisotazka]);
-
-	            if (odpoved.compareTo(kluc[cisotazka])==0){
-	                correct++;} 
-	            else {incorrect++;}
-	        }
-
-	        if (correct > 14){
-	            System.out.println("You passed.");
+	        if (body > 14){
+	            System.out.println("Test bol uspesny");
 	        } else {
-	           System.out.println("You failed.");
+	           System.out.println("Nespravil si test");
 	        }
-	        System.out.println("You have " + correct + " correct answers.");
-	        System.out.println("You have " + incorrect + " incorrect answers.");
+	        System.out.println("Mas " + body + " spravnych odpovedi.");
+
+
 
 	}
 
