dba909ede32709d82f0ec1309b36770923db82a9
Jakub Palarik <palarik.jakub@gmail.com>
2018-04-10 04:55:04 +0200

    KandO

---
+++ src/Testy/KluceOtazky.java
@@ -0,0 +1,29 @@
+package Testy;
+
+public class KluceOtazky {
+	
+	String[] zsfyzkluc = {"b","d","a","a","c"};
+	String[] zsmatkluc = {"100","101","2.5","10","1.1"};
+	String[] zsinfokluc = {"b","d","a","a","c"};
+	
+	String[] zsfyzotazka = {"b","d","a","a","c"};
+	String[] zsmatotazka = {"100","101","2.5","10","1.1"};
+	String[] zsinfootazka = {"b","d","a","a","c"};
+	
+	String[] ssfyzkluc = {"b","d","a","a","c"};
+	String[] ssmatkluc = {"100","101","2.5","10","1.1"};
+	String[] ssinfokluc = {"b","d","a","a","c"};
+	
+	String[] ssfyzotazka = {"b","d","a","a","c"};
+	String[] ssmatotazka = {"100","101","2.5","10","1.1"};
+	String[] ssinfootazka = {"b","d","a","a","c"};
+	
+	String[] vsfyzkluc = {"b","d","a","a","c"};
+	String[] vsmatkluc = {"100","101","2.5","10","1.1"};
+	String[] vsinfokluc = {"b","d","a","a","c"};
+	
+	String[] vsfyzotazka = {"b","d","a","a","c"};
+	String[] vsmatotazka = {"100","101","2.5","10","1.1"};
+	String[] vsinfootazka = {"b","d","a","a","c"};
+
+}
